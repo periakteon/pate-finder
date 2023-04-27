92b0a5f08af76afc9ac6cd7c6b501330da9895ae
Ege Aydemir <egeaydemirr97@gmail.com>
2023-04-28 00:10:31 +0300

    login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,159 @@
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookSquare,
+  faGoogle,
+  faSquareTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+
 const Login = () => {
-  
-  // TODO: Make login operation. use /api/login
-  
+  //TODO: Make register operation. use /api/register
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    termsAccepted: false,
+  });
+
+  const [showEmailError, setShowEmailError] = useState(false);
+  const [showPasswordError, setShowPasswordError] = useState(false);
+
+  const handleBlur = (event: React.ChangeEvent<HTMLInputElement>) => {
+    switch (event.target.name) {
+      case "email":
+        setShowEmailError(event.target.value.trim() === "");
+        break;
+      case "password":
+        setShowPasswordError(event.target.value.trim() === "");
+        break;
+      default:
+        break;
+    }
+  };
   return (
-    <div className="min-h-screen bg-gradient-to-br from-red-400 to-purple-600 flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8">
-      <div className="border-solid border-2 border-sky-500 w-56 backdrop-blur-sm bg-white/30 flex justify-center">
-        <h1 className="text-5xl">Hello</h1>
+    <div className=" min-h-screen bg-gradient-to-br from-cyan-400 to-purple-400 flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8 selection:bg-rose-500 selection:text-white">
+      <Image
+        src="/logo/png/logo-no-background.png"
+        width={125}
+        height={125}
+        alt="logo"
+        className="absolute flex self-start top-0 left-0 mt-5 ml-5"
+      />
+      <div className="absolute py-3 sm:mx-auto">
+        <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-purple-500 shadow-lg transform  sm:-rotate-3 sm:rounded-xl"></div>
+        <div className=" backdrop-blur-sm w-80 hover:max-h-screen bg-white/80 rounded-md p-8 m-1">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              console.log(formData);
+            }}
+          >
+            <h2 className="text-4xl mb-12 flex justify-center text-purple-900">
+              Giriş Yap
+            </h2>
+            <div className="relative mb-7 h-16">
+              <label
+                htmlFor="email"
+                className="block mb-1 font-bold text-gray-600"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={(e) => {
+                  setFormData((prevFormData) => ({
+                    ...prevFormData,
+                    email: e.target.value,
+                  }));
+                }}
+                onBlur={handleBlur}
+                className={`peer p-3 h-10 w-full text-sm border-b-2 ${showEmailError ? "border-red-500" : "border-gray-300"
+                  } text-gray-600 bg-white focus-within:outline-none focus-within:border-rose-600`}
+                placeholder="example@mail.com"
+                required
+              />
+              {showEmailError && (
+                <div className="text-red-500 text-xs">Email boş bırakılamaz.</div>
+              )}
+            </div>
+
+            <div className="relative mb-7 h-16">
+              <label
+                htmlFor="password"
+                className="block mb-1 font-bold text-gray-600"
+              >
+                Parola
+              </label>
+              <input
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={(e) => {
+                  setFormData((prevFormData) => ({
+                    ...prevFormData,
+                    password: e.target.value,
+                  }));
+                }}
+                onBlur={handleBlur}
+                className={`peer p-3 h-10 text-sm w-full border-b-2 ${showPasswordError ? "border-red-500" : "border-gray-300"
+                  } text-gray-600  focus-within:outline-none focus-within:border-rose-600`}
+                placeholder="*********"
+                required
+              />
+
+              {showPasswordError && (
+                <div className="text-red-500 text-xs">Parola boş bırakılamaz.</div>
+              )}
+            </div>
+
+
+            <div className="p-8" />
+            <div className="flex items-center justify-around pb-2">
+              <button className="h-12 w-12">
+                <FontAwesomeIcon
+                  icon={faFacebookSquare}
+                  className="w-8 h-8"
+                  color="#3B5998"
+                />
+              </button>
+              <button className="h-12 w-12">
+                <FontAwesomeIcon
+                  icon={faSquareTwitter}
+                  className="w-8 h-8"
+                  color="#1DA1F2"
+                />
+              </button>
+              <button className="h-12 w-12">
+                <FontAwesomeIcon
+                  icon={faGoogle}
+                  className="w-7 h-7"
+                  color="#DB4437"
+                />
+              </button>
+            </div>
+
+            <div className="flex flex-col items-center justify-center space-y-4">
+              <button
+                type="submit"
+                className="w-full bg-gradient-to-r from-rose-700 to-purple-700 hover:from-rose-500 hover:to-purple-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-300 ease-in-out"
+              >
+                Giris Yap
+              </button>
+              <div className="p-1" />
+              <div className="text-sm  text-gray-500">Hesabiniz yok mu?</div>
+              <Link href="/register">
+                <div className="text-rose-500 hover:text-purple-500 text-md duration-300 ease-in-out -mt-4">
+                  Kayit ol
+                </div>
+              </Link>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -59,7 +59,7 @@ const Register = () => {
         width={125}
         height={125}
         alt="logo"
-        className="flex self-start"
+        className="flex self-start absolute top-0 left-0 mt-5 ml-5"
       />
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
         <div className="absolute inset-0 bg-gradient-to-r from-purple-500 to-rose-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
@@ -94,8 +94,8 @@ const Register = () => {
               <label
                 htmlFor="username"
                 className={`absolute transition-all ${formData.username.length === 0 && !formData.username
-                    ? "left-2 top-2 text-gray-400 peer-focus-within:left-0 peer-focus-within:-top-6 peer-focus-within:text-gray-700 peer-focus-within:text-md peer-focus-within:text-bold"
-                    : "left-0 -top-6 text-gray-900 text-md "
+                  ? "left-2 top-2 text-gray-400 peer-focus-within:left-0 peer-focus-within:-top-6 peer-focus-within:text-gray-700 peer-focus-within:text-md peer-focus-within:text-bold"
+                  : "left-0 -top-6 text-gray-900 text-md "
                   }`}
               >
                 Kullanıcı Adı
@@ -127,8 +127,8 @@ const Register = () => {
               <label
                 htmlFor="email"
                 className={`absolute transition-all ${formData.email.length === 0 && !formData.email
-                    ? "left-2 top-2 text-gray-400 peer-focus-within:left-0 peer-focus-within:-top-6 peer-focus-within:text-gray-700 peer-focus-within:text-md peer-focus-within:text-bold"
-                    : "left-0 -top-6 text-gray-900 text-md "
+                  ? "left-2 top-2 text-gray-400 peer-focus-within:left-0 peer-focus-within:-top-6 peer-focus-within:text-gray-700 peer-focus-within:text-md peer-focus-within:text-bold"
+                  : "left-0 -top-6 text-gray-900 text-md "
                   }`}
               >
                 Email
@@ -158,8 +158,8 @@ const Register = () => {
               <label
                 htmlFor="password"
                 className={`absolute transition-all ${formData.password.length === 0 && !formData.password
-                    ? "left-2 top-2 text-gray-400 peer-focus-within:left-0 peer-focus-within:-top-6 peer-focus-within:text-gray-700 peer-focus-within:text-md peer-focus-within:text-bold"
-                    : "left-0 -top-6 text-gray-900 text-md "
+                  ? "left-2 top-2 text-gray-400 peer-focus-within:left-0 peer-focus-within:-top-6 peer-focus-within:text-gray-700 peer-focus-within:text-md peer-focus-within:text-bold"
+                  : "left-0 -top-6 text-gray-900 text-md "
                   }`}
               >
                 Parola
@@ -192,9 +192,9 @@ const Register = () => {
               <label
                 htmlFor="confirmPassword"
                 className={`absolute transition-all ${formData.confirmPassword.length === 0 &&
-                    !formData.confirmPassword
-                    ? "left-2 top-2 text-gray-400 peer-focus-within:left-0 peer-focus-within:-top-6 peer-focus-within:text-gray-700 peer-focus-within:text-md peer-focus-within:text-bold"
-                    : "left-0 -top-6 text-gray-900 text-md "
+                  !formData.confirmPassword
+                  ? "left-2 top-2 text-gray-400 peer-focus-within:left-0 peer-focus-within:-top-6 peer-focus-within:text-gray-700 peer-focus-within:text-md peer-focus-within:text-bold"
+                  : "left-0 -top-6 text-gray-900 text-md "
                   }`}
               >
                 Parola
