8b0f538632a65e7bf622ce065ed50f9cbd05d29d
Ege Aydemir <egeaydemirr97@gmail.com>
2023-05-07 21:31:17 +0300

    getPetsByUser zod validation

--- src/pages/api/pet/getPetsByUser.ts
+++ src/pages/api/pet/getPetsByUser.ts
@@ -1,41 +1,61 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient, Pet } from "@prisma/client";
+import { Pet, PrismaClient } from "@prisma/client";
 import authMiddleware from "@/middleware/authMiddleware";
+import { z } from "zod";
 
 const prisma = new PrismaClient();
 
 type ResponseType =
   | { success: true; message: string; pets: Pet[] }
-  | { success: false; error: string };
+  | { success: false; error: string[] };
+
+const getPetsByUserSchema = z.object({
+  userId: z
+    .string({
+      invalid_type_error: "Kullanıcı id'si sayı olmalıdır.",
+      required_error: "Kullanıcı id'si gereklidir.",
+    })
+    .transform(Number),
+});
 
 const handleGetPetsByUser = async (
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>,
 ) => {
-  const userId = req.query.userId;
+  const parsed = await getPetsByUserSchema.safeParseAsync(req.query);
+  console.log("parsed", parsed);
+  console.log("req.query:", req.query);
 
-  // TODO: ALARM ALARM no type validation with zod for user id
+  if (!parsed.success) {
+    const errorMap = parsed.error.flatten().fieldErrors;
+    const errorMessages = Object.values(errorMap).flatMap(
+      (errors) => errors ?? [],
+    );
+    return res.status(400).json({ success: false, error: errorMessages });
+  }
+  const { userId } = parsed.data;
+  console.log("userId:", userId);
+
   /**
    * http://localhost:3000/api/pet/getPetsByUser?userId=1
    */
   if (req.method !== "GET") {
     return res
       .status(405)
-      .json({ success: false, error: "Method not allowed" });
+      .json({ success: false, error: ["Method not allowed"] });
   }
-  if (req.method === "GET") {
-    try {
-      const pets = await prisma.pet.findMany({
-        where: {
-          ownerId: parseInt(userId as string),
-        },
-      });
-      res
-        .status(200)
-        .json({ success: true, message: "Petler başarıyla getirildi.", pets });
-    } catch (error) {
-      res.status(500).json({ success: false, error: "Internal Server Error" });
-    }
+
+  try {
+    const pets = await prisma.pet.findMany({
+      where: {
+        ownerId: userId,
+      },
+    });
+    res
+      .status(200)
+      .json({ success: true, message: "Petler başarıyla getirildi.", pets });
+  } catch (error) {
+    res.status(500).json({ success: false, error: ["Internal Server Error"] });
   }
 };
 
