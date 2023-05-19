ba8f5f9a17cfc32fcb995074efbf669ea36033ec
periakteon <masumgokyuz@gmail.com>
2023-05-19 03:30:14 +0300

    dark mode added

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -7,12 +7,17 @@ import {
   faPaw,
   faUser,
   faBars,
+  faSun,
+  faMoon
 } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
+import { useTheme } from 'next-themes';
 
 const Sidebar = () => {
   const [searchMode, setSearchMode] = useState<boolean>(false);
   const searchInputRef = useRef<HTMLInputElement | null>(null);
+
+  const { resolvedTheme, theme, setTheme } = useTheme();
 
   const handleSearchClick = () => {
     setSearchMode(true);
@@ -43,7 +48,7 @@ const Sidebar = () => {
   }, []);
 
   return (
-    <div className="bg-violet-50 flex flex-col justify-between w-64 h-screen sticky top-0">
+    <div className="bg-violet-50 flex flex-col justify-between w-64 h-screen sticky top-0 border-2 border-r-pink-200 dark:bg-dark-primary dark:border-2 dark:border-r-violet-500">
       <div className="p-4 overflow-y-auto max-h-screen-80">
         <div className="text-3xl font-bold text-center mb-6 flex items-center justify-center">
           <Link href="/feed">
@@ -98,7 +103,7 @@ const Sidebar = () => {
                       type="text"
                       id="searchInput"
                       placeholder="Ara..."
-                      className="w-full p-2 pr-10 rounded-lg border-2 border-pink-600"
+                      className="w-full p-2 pr-10 rounded-lg border-2 border-pink-600 dark:bg-pink-200 dark:text-black"
                     />
                     <FontAwesomeIcon
                       icon={faMagnifyingGlass}
@@ -129,15 +134,28 @@ const Sidebar = () => {
           </ul>
         </nav>
       </div>
-      <div className="p-4">
-        <div className="flex items-center p-4 text-pink-600 hover:bg-pink-200 rounded-lg cursor-pointer">
-          <FontAwesomeIcon
-            icon={faBars}
-            className="text-2xl text-pink-600 mr-2"
-          />
-          <span className="text-lg font-medium">Ayarlar</span>
-        </div>
-      </div>
+      <div className="p-4 mx-auto">
+  <div className="flex items-center p-4 text-pink-600 rounded-lg">
+    <button className="transition-all cursor-pointer">
+      {resolvedTheme === "dark" ? (
+        <FontAwesomeIcon
+          icon={faSun}
+          className={`icon-style mr-2 ${theme === "light" ? "rotate-0" : "rotate-90"} transition-transform animate-spin-slow`}
+          onClick={() => setTheme("light")}
+          style={{ fontSize: "3rem" }} // İstediğiniz boyutu burada belirleyin
+        />
+      ) : (
+        <FontAwesomeIcon
+          icon={faMoon}
+          className={`icon-style mr-2 ${theme === "dark" ? "rotate-0" : "rotate-0"} transition-transform animate-spin-slow`}
+          onClick={() => setTheme("dark")}
+          style={{ fontSize: "3rem" }} // İstediğiniz boyutu burada belirleyin
+        />
+      )}
+    </button>
+  </div>
+</div>
+
     </div>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,7 @@ import { Poppins } from "next/font/google";
 import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-const roboto = Roboto({
-  weight: "400",
-  subsets: ["latin"],
-});
+import { ThemeProvider } from "next-themes";
 
 const poppins = Poppins({
   weight: "400",
@@ -17,10 +14,12 @@ const poppins = Poppins({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <ToastContainer />
-      <main className={poppins.className}>
-        <Component {...pageProps} />
-      </main>
+      <ThemeProvider attribute="class">
+        <ToastContainer />
+        <main className={`${poppins.className} bg-gray-100 dark:bg-dark-secondary`}>
+          <Component {...pageProps} />
+        </main>
+      </ThemeProvider>
     </>
   );
 }
