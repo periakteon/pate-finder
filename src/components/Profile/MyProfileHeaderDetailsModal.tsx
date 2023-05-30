a83c3ed474c5f698989684ed0c16240a978bc021
periakteon <masumgokyuz@gmail.com>
2023-05-30 17:46:39 +0300

    modal devs

--- src/components/Profile/MyProfileHeaderDetailsModal.tsx
+++ src/components/Profile/MyProfileHeaderDetailsModal.tsx
@@ -12,10 +12,10 @@ Modal.setAppElement("#__next");
 const MyProfileHeaderDetailsModal: React.FC = () => {
   const [myProfile, setMyProfile] = useAtom(myProfileAtom);
   const [isHeaderDetailsModalOpen, setIsHeaderDetailsModalOpen] = useAtom(
-    isHeaderDetailsModalOpenAtom
+    isHeaderDetailsModalOpenAtom,
   );
   const [activeTab, setActiveTab] = useState<"followers" | "following" | "pet">(
-    "followers"
+    "followers",
   );
 
   if (!myProfile) {
@@ -24,34 +24,60 @@ const MyProfileHeaderDetailsModal: React.FC = () => {
 
   const renderFollowers = () => {
     return myProfile.followedBy.map((follower) => (
-      <div key={follower.follower.username}>
-        <h3>{follower.follower.username}</h3>
-        <Image src={follower.follower.profile_picture || "/images/default.jpeg"} alt="Profile Picture" width={64} height={64}/>
+      <div key={follower.follower.username} className="flex items-center my-3">
+        <div className="mr-2">
+          <Image
+            src={follower.follower.profile_picture || "/images/default.jpeg"}
+            alt="Profile Picture"
+            width={64}
+            height={64}
+            className="rounded-full"
+          />
+        </div>
+        <h3 className="">{follower.follower.username}</h3>
       </div>
     ));
   };
 
   const renderFollowing = () => {
     return myProfile.following.map((followed) => (
-      <div key={followed.following.username}>
+      <div key={followed.following.username} className="flex items-center my-3">
+        <div className="mr-2">
+          <Image
+            src={followed.following.profile_picture || "/images/default.jpeg"}
+            alt="Profile Picture"
+            width={64}
+            height={64}
+            className="rounded-full"
+          />
+        </div>
         <h3>{followed.following.username}</h3>
-        <Image src={followed.following.profile_picture || "/images/default.jpeg"} alt="Profile Picture" width={64} height={64}/>
       </div>
     ));
   };
 
   const renderPet = () => {
     if (!myProfile.pet) {
-      return <p>No pet information available.</p>;
+      return <p>Pet bilgisi mevcut değil.</p>;
     }
 
     return (
-      <div>
-        <h3>{myProfile.pet.name}</h3>
-        <p>Breed: {myProfile.pet.breed}</p>
-        <Image src={myProfile.pet.pet_photo || "/images/default.jpeg"} alt="Pet Photo" width={64} height={64}/>
-        <p>Type: {myProfile.pet.type}</p>
-        <p>Bio: {myProfile.pet.bio}</p>
+      <div className="flex items-center my-3">
+        <div className="mr-2">
+          <Image
+            src={myProfile.pet.pet_photo || "/images/default.jpeg"}
+            alt="Pet Photo"
+            width={64}
+            height={64}
+            className="rounded-full"
+          />
+        </div>
+        <div>
+          <h3>{myProfile.pet.name}</h3>
+          <p>Breed: {myProfile.pet.breed}</p>
+          <p>Type: {myProfile.pet.type}</p>
+          <p>Bio: {myProfile.pet.bio}</p>
+        </div>
       </div>
     );
   };
@@ -78,37 +104,43 @@ const MyProfileHeaderDetailsModal: React.FC = () => {
       className="fixed inset-0 flex items-center justify-center overflow-auto z-[150]"
       overlayClassName="fixed inset-0 bg-black bg-opacity-50 z-[151]"
     >
-      <div className="w-2/3 h-full bg-dark-dropzone flex overflow-y-scroll">
-      <button
-            className="absolute top-4 right-4 bg-transparent rounded-full p-2 hover:bg-gray-200 transition duration-300 focus:outline-none"
-            onClick={() => setIsHeaderDetailsModalOpen(false)}
-          >
-            <span className="h-6 w-6 text-gray-500 hover:text-gray-700">
-              <FontAwesomeIcon icon={faTimes} />
-            </span>
-          </button>
-        <div className="w-full">
-          <div className="flex justify-center mb-4">
+      <div className="h-full w-1/4 bg-dark-dropzone flex overflow-y-scroll">
+        <button
+          className="absolute top-4 right-4 bg-transparent rounded-full p-2 hover:bg-gray-200 transition duration-300 focus:outline-none"
+          onClick={() => setIsHeaderDetailsModalOpen(false)}
+        >
+          <span className="h-6 w-6 text-gray-500 hover:text-gray-700">
+            <FontAwesomeIcon icon={faTimes} />
+          </span>
+        </button>
+        <div className="w-full h-screen overflow-y-scroll">
+          <div className="min-w-screen flex justify-center mb-4">
             <button
               className={`${
-                activeTab === "followers" ? "bg-blue-500 text-white" : "bg-gray-300 text-gray-700"
-              } px-4 py-2 rounded-tl-lg`}
+                activeTab === "followers"
+                  ? "bg-blue-500 text-white"
+                  : "bg-gray-300 text-gray-700"
+              } px-4 py-2 rounded-tl-lg h-full flex-1`}
               onClick={() => setActiveTab("followers")}
             >
               Takipçiler
             </button>
             <button
               className={`${
-                activeTab === "following" ? "bg-blue-500 text-white" : "bg-gray-300 text-gray-700"
-              } px-4 py-2`}
+                activeTab === "following"
+                  ? "bg-blue-500 text-white"
+                  : "bg-gray-300 text-gray-700"
+              } px-4 py-2 h-full flex-1`}
               onClick={() => setActiveTab("following")}
             >
-              Takip Edilenler
+              Takip
             </button>
             <button
               className={`${
-                activeTab === "pet" ? "bg-blue-500 text-white" : "bg-gray-300 text-gray-700"
-              } px-4 py-2 rounded-tr-lg`}
+                activeTab === "pet"
+                  ? "bg-blue-500 text-white"
+                  : "bg-gray-300 text-gray-700"
+              } px-4 py-2 rounded-tr-lg h-full flex-1`}
               onClick={() => setActiveTab("pet")}
             >
               Pet
